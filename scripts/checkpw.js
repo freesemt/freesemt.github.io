121a23f0dc5159a84796b0720b60ba19bfa74c35
freesemt <freesemt@gmail.com>
2025-02-23 16:14:34 +0900

    added the dialog pages

--- scripts/checkpw.js
+++ scripts/checkpw.js
@@ -1,17 +1,32 @@
 // Include the CryptoJS library
 
-function checkPassword(hashedPassword) {
+function checkPassword(correctHash, showHash, redirectUrl, hashMemoryName) {
+    var storedHash = localStorage.getItem(hashMemoryName);
+    if (showHash) {
+        alert("Stored hash: " + storedHash);
+    }
+    
+    if (storedHash === correctHash) {
+        document.getElementById('protected-content').style.display = 'block';
+        return;
+    }
+
     var password = prompt("Please enter the password:");
     if (password === null || password === "") {
         alert("Password is required!");
-        window.location.href = "../index.html"; // Redirect to the main page
+        window.location.href = redirectUrl; // Redirect to the specified page
         return;
     }
+
     var hash = CryptoJS.SHA256(password).toString();
-    if (hash !== hashedPassword) {
-        alert("Incorrect password!");
-        window.location.href = "../index.html"; // Redirect to the main page
-    } else {
+    if (showHash) {
+        alert("The hashed password is: " + hash);
+    }
+    if (hash === correctHash) {
+        localStorage.setItem(hashMemoryName, hash);
         document.getElementById('protected-content').style.display = 'block';
+    } else {
+        alert("Incorrect password!");
+        window.location.href = redirectUrl; // Redirect to the specified page
     }
 }
